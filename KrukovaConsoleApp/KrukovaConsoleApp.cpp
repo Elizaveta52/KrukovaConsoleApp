a619d0db2b4903a58a6efeed25a6091ab6549041
Elizaveta52 <e.s.krukova@yandex.ru>
2026-09-07 14:25:57 +0300

    Добавление переменной

--- KrukovaConsoleApp/KrukovaConsoleApp.cpp
+++ KrukovaConsoleApp/KrukovaConsoleApp.cpp
@@ -4,8 +4,10 @@ int main()
 {
     std::cout << "Hello world" << std::endl;
 
-    std::cout << "Press any key to continue . . . ";
-    std::cin.get();
+    //std::cout << "Press any key to continue...";
+    int x;
+    std::cin >> x;
+    std::cout << 10 / x;
 
     return 0;
 }
